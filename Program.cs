b123f8a0f1051a1e651f3c4db77231dceb62e457
Sage Klein <sageklein@gmail.com>
2020-11-11 14:16:55 -0600

    moose asks a yes or no question

--- Program.cs
+++ Program.cs
@@ -4,9 +4,85 @@ namespace MagicMoose
 {
     class Program
     {
+        static bool MooseAsks(string question)
+        {
+            Console.Write($"{question} (Y/N): ");
+            string answer = Console.ReadLine().ToLower();
+
+            while (answer != "y" && answer != "n")
+            {
+                Console.Write($"{question} (Y/N): ");
+                answer = Console.ReadLine().ToLower();
+            }
+
+            if (answer == "y")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome, I'm THE MAGIC MOOSE!");
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine();
+
+            // Let the moose speak!
+            MooseSays("Ask your question my child.");
+
+            // As a question
+            Question();
+        }
+
+
+        static void Question()
+        {
+            bool isTrue = MooseAsks("QUESTION");
+            if (isTrue)
+            {
+                MooseSays("NO ANSWER");
+            }
+            else
+            {
+                MooseSays("YES ANSWER");
+            }
+        }
+
+        static void MooseSays(string message)
+
+
+        {
+            Console.WriteLine($@"
+                                       _.--^^^--,
+                                    .'          `\
+  .-^^^^^^-.                      .'              |
+ /          '.                   /            .-._/
+|             `.                |             |
+ \              \          .-._ |          _   \
+  `^^'-.         \_.-.     \   `          ( \__/
+        |             )     '=.       .,   \
+       /             (         \     /  \  /
+     /`               `\        |   /    `'
+     '..-`\        _.-. `\ _.__/   .=.
+          |  _    / \  '.-`    `-.'  /
+          \_/ |  |   './ _     _  \.'
+               '-'    | /       \ |
+                      |  .-. .-.  |
+                      \ / o| |o \ /
+                       |   / \   |    {message}
+                      / `^`   `^` \
+                     /             \
+                    | '._.'         \
+                    |  /             |
+                     \ |             |
+                      ||    _    _   /
+                      /|\  (_\  /_) /
+                      \ \'._  ` '_.'
+                       `^^` `^^^`
+            ");
         }
     }
 }
